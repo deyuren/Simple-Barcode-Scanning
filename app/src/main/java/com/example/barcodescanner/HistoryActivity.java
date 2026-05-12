42f960d8043e0c3ec70ec55a5862161aec884da7
Builder <build@example.com>
2026-05-12 02:53:18 +0000

    fix: catch Exception in HistoryActivity

--- app/src/main/java/com/example/barcodescanner/HistoryActivity.java
+++ app/src/main/java/com/example/barcodescanner/HistoryActivity.java
@@ -87,7 +87,7 @@ public class HistoryActivity extends AppCompatActivity {
                 JSONArray entry = arr.getJSONArray(i);
                 historyList.add(new String[]{entry.getString(0), entry.getString(1)});
             }
-        } catch (JSONException e) {
+        } catch (Exception e) {
             Log.e("HistoryActivity", "loadHistory error", e);
         }
     }
@@ -103,7 +103,7 @@ public class HistoryActivity extends AppCompatActivity {
             }
             getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
                     .edit().putString(KEY_HISTORY, arr.toString()).apply();
-        } catch (JSONException e) {
+        } catch (Exception e) {
             Log.e("HistoryActivity", "persistHistory error", e);
         }
     }
